090cd13d88e8978d2fc8dce683ffa7974368b0ba
tanaymehto <tanaymehto21@gmail.com>
2026-07-02 01:41:11 +0530

    tikdam

---
+++ config.py
@@ -0,0 +1,17 @@
+EMAIL="23f2004044@ds.study.iitm.ac.in"
+
+Q1_ALLOWED_ORIGIN="https://dash-z4rtw7.example.com"
+
+ISSUER="https://idp.exam.local"
+
+AUDIENCE="tds-3cc1qo8b.apps.exam.local"
+
+Q5_API_KEY="ak_lmtz65rvelcrrp0x1p42smgb"
+
+Q9_TOTAL_ORDERS=41
+
+Q9_RATE_LIMIT=17
+
+Q10_ALLOWED_ORIGIN="https://app-n4erjq.example.com"
+
+Q10_RATE_LIMIT=12
