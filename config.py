e1432d477bae6c5f9056eca46e877e0ff7159a1e
tanaymehto <tanaymehto21@gmail.com>
2026-07-02 01:56:28 +0530

    galti

--- config.py
+++ config.py
@@ -1,17 +1,31 @@
-EMAIL="23f2004044@ds.study.iitm.ac.in"
+EMAIL = "23f2004044@ds.study.iitm.ac.in"
 
-Q1_ALLOWED_ORIGIN="https://dash-z4rtw7.example.com"
+Q1_ALLOWED_ORIGIN = "https://dash-z4rtw7.example.com"
 
-ISSUER="https://idp.exam.local"
+ISSUER = "https://idp.exam.local"
+AUDIENCE = "tds-3cc1qo8b.apps.exam.local"
 
-AUDIENCE="tds-3cc1qo8b.apps.exam.local"
+PUBLIC_KEY_PEM = """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----"""
 
-Q5_API_KEY="ak_lmtz65rvelcrrp0x1p42smgb"
+Q3_PORT = 8845
+Q3_WORKERS = 10
+Q3_DEBUG = False
+Q3_LOG_LEVEL = "info"
 
-Q9_TOTAL_ORDERS=41
+Q5_API_KEY = "ak_lmtz65rvelcrrp0x1p42smgb"
 
-Q9_RATE_LIMIT=17
+Q9_TOTAL_ORDERS = 41
+Q9_RATE_LIMIT = 17
 
-Q10_ALLOWED_ORIGIN="https://app-n4erjq.example.com"
+Q10_ALLOWED_ORIGIN = "https://app-n4erjq.example.com"
+Q10_RATE_LIMIT = 12
 
-Q10_RATE_LIMIT=12
+EXAM_PORTAL_ORIGIN = "https://exam.sanand.workers.dev"
